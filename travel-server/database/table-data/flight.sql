66cce3c95ff76203984493d2a15603f38272e445
Jeroen de Jong <jdjjong@icloud.com>
2019-07-31 22:33:20 +0100

    DATA: added remaining flights I've taken

--- travel-server/database/table-data/flight.sql
+++ travel-server/database/table-data/flight.sql
@@ -14,3 +14,59 @@ VALUES
 (13,27188,5653,NULL,E'2019-05-04 10:25:00',E'2019-05-04 13:40:00',4,E'CA135'),
 (14,5642,27188,NULL,E'2019-05-14 12:45:00',E'2019-05-14 14:15:00',4,E'CA130'),
 (15,27188,2434,NULL,E'2019-05-14 16:25:00',E'2019-05-14 20:40:00',4,E'CA855');
+
+---- Still to do: (its not valid SQL i know but i shall still commit xD)
+
+2017/04/05 - 16:25 - 21:35
+London Stansted - Sofia - Ryanair - FR9967
+
+2017/04/15 - ????
+Sofia - Bristol - Wizzair - WZZ4318
+
+2017/06/17 - 14:00 - 16:25
+Southampton - Amsterdam - Flybe - BE1015
+
+2017/06/17 - 17:35 - 17:55
+Gronigen - London Southend - Flybe (operated by Stobart air) - BE6037 
+
+2017/11/02 - 13:45 - 16:05 
+Amsterdam - Bristol - KLM - KL1052
+
+2017/11/10 - 15:45 - 18:05
+London Gatwick - Amsterdam - British Airways - BA2762
+
+2017/11/12 - 17:20 - 17:40
+Gronigen - London Southend - Flybe (operated by Stobart air) - BE6037 
+
+2017/12/23 - 14:45 - 17:10
+Southampton - Amsterdam - Flybe - BE1015
+
+2017/12/23 - 17:15 - 17:35
+Amsterdam - Southampton - Flybe - BE1016
+
+2018/03/29 - 10:30 - 12:50
+Southampton - Amsterdam - Flybe - BE1013
+
+2018/04/03 - 17:20 - 17:40
+Groningen - London Southend - Flybe (operated by Stobart air) - BE6037
+
+2018/05/25 - 09:30 - 23:25
+London Heathrow - Mumbai - Jet Airways - 9W119
+
+2018/05/26 - 01:10 - 09:40
+Mumbai - Hong Kong - Jet Airways - 9W76
+
+2018/08/10 - 15:30 - 22:15
+  15:30 - 16:15
+  Helsinki - Copenhagen - SAS (Operated by CityJet) - SK1713
+  21:10 - 22:15
+  Copenhagen - Groningen - SAS (Operated by LOT polish Airline) - SK8667
+
+2018/08/13 - 17:15 - 17:40
+Amsterdam - London Gatwick - EasyJet - EZY8878
+ 
+2018/10/19 - 14:25 - 16:40
+London Southend - Amsterdam - Flybe (operated by Stobart Air) - BE6034
+
+2018/10/21 - 19:20 - 19:35
+Amsterdam - Bristol - EasyJet - EZY616
