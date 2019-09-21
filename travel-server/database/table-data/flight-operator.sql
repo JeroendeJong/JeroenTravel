d4be81a543d08a8cf5fca0d00d137c2f7e7c63c5
Jeroen <jdjjong@hotmail.com>
2019-09-21 13:10:54 +0100

    DATA: keep flights and operators up-to-date

--- travel-server/database/table-data/flight-operator.sql
+++ travel-server/database/table-data/flight-operator.sql
@@ -3,4 +3,11 @@ VALUES
 (1,E'Vueling'),
 (2,E'Ryanair'),
 (3,E'Flybe'),
-(4,E'Air China');
+(4,E'Air China'),
+(5,E'Wizz Air'),
+(6,E'KLM'),
+(7,E'British Airways'),
+(8,E'Jet Airways'),
+(9,E'SAS'),
+(10,E'Easy Jet'),
+(11,E'AirBaltic');
