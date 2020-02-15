61b251f1c21259329a0c521e98e7553336fb1be5
Jeroen <jdjjong@hotmail.com>
2020-02-15 12:32:07 +0000

    MISC: remove stub text and explain why no text + change lazy mode text

--- travel-frontend/src/UI/travel/main-screens/segments-list/timeline/vertical-tldr-timeline.tsx
+++ travel-frontend/src/UI/travel/main-screens/segments-list/timeline/vertical-tldr-timeline.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const LazyContainer = styled.div`
+  text-align: center;
+  font-size: 16px;
+`
 
 const VerticalTLDRTimeline = () => {
   return (
-    <div>
-      You lazy Twat
-    </div>
+    <LazyContainer>
+      <br/><br/><br/>
+      Lazy Mode has not been implemented yet :(.
+      <br/>
+      So; be un-lazy <span aria-label="emoji" role="img">😅</span>
+    </LazyContainer>
   );
 };
 
--- travel-frontend/src/UI/travel/main-screens/trip-segment.tsx
+++ travel-frontend/src/UI/travel/main-screens/trip-segment.tsx
@@ -11,25 +11,7 @@ import { TripOverview, TripDetail, TripSegment } from '../types';
 import { coordinatesToBounds, centreOnBounds } from '../../../map/utils';
 
 const TEST_TEXT = `
-![Image 1, what a great image innit mate.!](${getImageUrl('/assets/IMG_0334.jpg')}  "sup browksies")
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim arcu ante, vel aliquet nibh dictum non. In dictum maximus eros id dictum. Aliquam tristique tristique felis convallis rutrum. Suspendisse convallis lectus in nunc posuere finibus quis quis odio. Nulla commodo lacinia est, eu rutrum odio luctus quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam non convallis nulla.
-
-![Image 1, what a great image innit mate.!](${getImageUrl('/assets/IMG_2569.jpg')})
-
-Morbi a orci id ligula rhoncus mollis. Donec malesuada enim turpis. Nam ante lacus, molestie vitae metus sit amet, bibendum lacinia arcu. Sed id tellus ut nunc bibendum tincidunt finibus a turpis. Maecenas volutpat placerat lacinia. Proin id vehicula nulla. Donec nec placerat libero, vel imperdiet turpis. Donec molestie lacus justo, ut vehicula ipsum ullamcorper non.
-
-Sed a porta turpis, eu eleifend enim. Fusce ut nunc eros. Maecenas efficitur sem non aliquet elementum. Cras facilisis finibus odio, tincidunt vulputate odio interdum ac. Proin nisi neque, tempus vitae imperdiet pulvinar, pellentesque a nisl. Sed in semper nisl. Pellentesque nec ex fermentum, bibendum tellus at, tempus leo. Nunc tempus vulputate neque sed aliquet. Duis ipsum metus, pharetra sit amet feugiat vitae, eleifend id enim. Nulla dui velit, blandit vel volutpat ac, vulputate ac diam. Praesent posuere, massa quis mattis consectetur, diam odio sollicitudin lectus, vel condimentum libero mi nec metus. Vivamus faucibus enim in semper facilisis. Aliquam neque sapien, varius eu nunc ut, egestas lobortis purus. Ut tincidunt, urna ac congue scelerisque, orci lectus ultricies lorem, in placerat leo purus at mi.
-
-Aenean venenatis ex in nisi semper, ut ultricies lectus scelerisque. Donec accumsan ullamcorper cursus. Donec molestie sem et felis tristique tempus. Mauris rutrum massa eget pretium dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin sed ligula nec velit ullamcorper iaculis. Sed nec ex eget odio lacinia venenatis vitae vitae leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at massa molestie nisl feugiat semper. Phasellus porttitor fringilla fermentum. In at pretium ligula, quis rutrum sapien.
-
-![Image 1, what a great image innit mate.!](${getImageUrl('/assets/IMG_2041.jpg')}) 
-
-Aenean venenatis ex in nisi semper, ut ultricies lectus scelerisque. Donec accumsan ullamcorper cursus. Donec molestie sem et felis tristique tempus. Mauris rutrum massa eget pretium dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin sed ligula nec velit ullamcorper iaculis. Sed nec ex eget odio lacinia venenatis vitae vitae leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at massa molestie nisl feugiat semper. Phasellus porttitor fringilla fermentum. In at pretium ligula, quis rutrum sapien.
-
-Aenean venenatis ex in nisi semper, ut ultricies lectus scelerisque. Donec accumsan ullamcorper cursus. Donec molestie sem et felis tristique tempus. Mauris rutrum massa eget pretium dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin sed ligula nec velit ullamcorper iaculis. Sed nec ex eget odio lacinia venenatis vitae vitae leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at massa molestie nisl feugiat semper. Phasellus porttitor fringilla fermentum. In at pretium ligula, quis rutrum sapien.
-
-![Image 1, what a great image innit mate.!](${getImageUrl('/assets/IMG_0511.jpg')})
+  Story yet to be written. Sorry.
 `;
 
 const SegmentTitle = styled.div`
