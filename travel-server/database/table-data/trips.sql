562aa8d264575626415f201c5c8140f21b7900b1
Jeroen <jdjjong@hotmail.com>
2020-02-15 13:03:12 +0000

    SQL: DATA: update sql scripts with latest data sets

--- travel-server/database/table-data/trips.sql
+++ travel-server/database/table-data/trips.sql
@@ -1,5 +1,7 @@
 INSERT INTO "public"."trips"("id","name","description","country_codes","header_image_url","active","extent")
 VALUES
-(1,E'\'Round the Caucasus ',E'Solo travelling through former USSR countries Georgia And Azerbaijan.',E'AZ, GE',E'/assets/caucasus_header.jpg',FALSE,NULL),
-(2,E'Sicilian Mafia?',E'Summer Family escape to Sicily. Basically drinking coffee and eating Pizza.',E'IT',E'/assets/naples_header.jpg',TRUE,NULL),
-(3,E'Big Trip - Asia ',E'First big backpacking trip through Asia. Hong Kong to Helsinki all by land.',E'HK, CN, MN, RU, FI',NULL,FALSE,NULL);
+(1,E'\'Round the Caucasus ',E'Solo travelling through former USSR countries Georgia And Azerbaijan.',E'AZ, GE',E'/caucasus_header.jpg',FALSE,E'POLYGON((41.64562 40.420048,41.64562 43.061322,49.796784 43.061322,49.796784 40.420048,41.64562 40.420048))'),
+(2,E'Sicilian Mafia?',E'Summer Family escape to Sicily. Basically drinking coffee and eating Pizza.',E'IT',E'/naples_header.jpg',FALSE,E'POLYGON((13.114104373615 37.445633868,13.114104373615 40.851646,15.1027371013691 40.851646,15.1027371013691 37.445633868,13.114104373615 37.445633868))'),
+(4,E'Short South Korea',E'A short but very packed holiday travelling through Korea with my friends Maria and Martina',E'KR',NULL,FALSE,NULL),
+(5,E'Sunny Malta',E'Long weekend spend in Malta. Lots of driving, but amazing beaches and scenery!',E'MT',NULL,FALSE,NULL),
+(9,E'Winter sucks, lets go on holiday',E'Four days escape to the sun thanks to cheap flight deals.',E'MA',E'/IMG_0099.jpg',TRUE,NULL);
